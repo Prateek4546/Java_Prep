28e188444400539430972c958b3b8771288fffab
Prateek Giri <prateekgiri999@gmail.com>
2025-02-17 23:37:18 +0530

    java Pattern1

--- src/objectOrientedProgramming/multithreading/Counter.java
+++ src/objectOrientedProgramming/multithreading/Counter.java
@@ -1,12 +1,18 @@
 package objectOrientedProgramming.multithreading;
 
-public class Counter {
+public class Counter extends Thread {
     private int count;
 
-    public synchronized   void increment(){
+    public  void increment(){
         count++;
     }
     public int getCount() {
         return count;
+    }
+    @Override
+    public  void run(){
+        for(int i=0; i<10; i++){
+            increment();
+        }
     }
 }
--- src/objectOrientedProgramming/multithreading/Main.java
+++ src/objectOrientedProgramming/multithreading/Main.java
@@ -2,19 +2,16 @@ package objectOrientedProgramming.multithreading;
 
 
 public class Main {
-    public static void main(String[] args)  {
-      BankAccount bankAccount = new BankAccount();
-      Runnable task = new Runnable() {
-          @Override
-          public void run() {
-              bankAccount.withdraw(50);
-          }
-      };
-      Thread t1 = new Thread(task , "Thread 1");
-      Thread t2 = new Thread(task , "Thread 2");
+    public static void main(String[] args) throws InterruptedException {
+     Counter counter = new Counter();
+
+      Thread t1 = new Thread(counter , "Thread 1");
+      Thread t2 = new Thread(counter , "Thread 2");
+
       t1.start();
       t2.start();
-
-        System.out.println(bankAccount.getBalance());
+        
+        System.out.println(counter.getCount());
+        //github workflow for testing
     }
 }
